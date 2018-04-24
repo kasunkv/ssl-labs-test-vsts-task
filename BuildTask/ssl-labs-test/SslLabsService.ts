52f980301ef80dc6271fe317d2b43b264aff7b58
Kasun Kodagoda <kvkrusl@gmail.com>
2018-04-24 21:23:46 +0530

    Completed the implementation for the SslLabsService

--- BuildTask/ssl-labs-test/SslLabsService.ts
+++ BuildTask/ssl-labs-test/SslLabsService.ts
@@ -4,8 +4,10 @@ import { inject, injectable } from 'inversify';
 import { ITaskInput } from './interfaces/ITaskInput';
 import { ISslLabsService } from './interfaces/ISslLabsService';
 
-import TYPES from './di/types';
+import { Options } from 'node-ssllabs';
+import { TaskInput } from './TaskInput';
 
+import TYPES from './di/types';
 
 @injectable()
 export class SslLabsService implements ISslLabsService {
@@ -18,9 +20,22 @@ export class SslLabsService implements ISslLabsService {
     }
 
     executeSslTest(): Promise<string> {
-        const promise = new Promise((resolve, reject) => {
+        const promise = new Promise<string>((resolve, reject) => {
             try {
-                
+
+                const options: Options = new Options();
+                options.host = this._taskInput.Hostname;
+                options.maxAge = 24;
+                options.publish = this._taskInput.PublishScanResults;
+                options.startNew = this._taskInput.FreshScan;
+                options.all = 'done';                
+
+                SslLabs.scan(options, (err: any, res: any) => {
+                    if (err) {
+                        reject(err.message || err);
+                    }
+                    resolve(res);
+                });
             } catch (err) {
                 reject(err.message || err);
             }
