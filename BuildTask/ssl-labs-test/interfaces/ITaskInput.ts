f322e52c647daaec238cc73ca52992e49fca121d
Kasun Kodagoda <kvkrusl@gmail.com>
2018-04-24 21:23:24 +0530

    Completed the ITaskInput interface

--- BuildTask/ssl-labs-test/interfaces/ITaskInput.ts
+++ BuildTask/ssl-labs-test/interfaces/ITaskInput.ts
@@ -1,4 +1,14 @@
 export interface ITaskInput {
+    Hostname: string;
+    FreshScan: boolean;
+    PublishScanResults: boolean;
+    EnableVerification: boolean;
+    MinimumCertGrade: string;
+    EnableExpirationAlert: boolean;
+    DaysBeforeExpiration: number;
+    AlertMode: string;
+    VariableName: string;
+    VariableContent: string;
 
     toJSON(): string;
 }
