ef60c4a7e85fbc269f721c8255767768bd447219
Kasun Kodagoda <kvkrusl@gmail.com>
2018-07-08 00:01:58 +0530

    Added a Constants file

---
+++ BuildTask/ssl-labs-test/Constants.ts
@@ -0,0 +1,26 @@
+export class AlertMode {
+    static BREAK_BUILD: string = 'BreakBuild';
+    static SET_VARIABLE: string = 'SetVariable';
+}
+
+export class CertificateGradeScore {
+    static A_PLUS: Number = 100;
+    static A: Number = 90;
+    static A_MINUS: Number = 80;
+    static B: Number = 70;
+    static C: Number = 60;
+    static D: Number = 50;
+    static E: Number = 40;
+    static F: Number = 30;
+}
+
+export class CertificateGrade {
+    static A_PLUS: string = 'A+';
+    static A: string = 'A';
+    static A_MINUS: string = 'A-';
+    static B: string = 'B';
+    static C: string = 'C';
+    static D: string = 'D';
+    static E: string = 'E';
+    static F: string = 'F';
+}
